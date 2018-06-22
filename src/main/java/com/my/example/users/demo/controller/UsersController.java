fd53e2cd247879692f9e055a0e9388302f927f4d
Valera <valera7979@mail.ru>
2018-06-22 16:07:46 +0300

    Delete added

--- src/main/java/com/my/example/users/demo/controller/UsersController.java
+++ src/main/java/com/my/example/users/demo/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.my.example.users.demo.controller;
 import com.my.example.users.demo.model.User;
 import com.my.example.users.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,17 @@ public class UsersController {
 	public Iterable<User> findByName(@PathVariable("name") String name) {
 
 		return repository.findByName(name);
+	}
+
+	@DeleteMapping(value="/user/{id}")
+	public void deleteById(@PathVariable("id") long id) {
+
+		repository.deleteById(id);
+	}
+
+	@DeleteMapping(value="/users")
+	public void deleteAll() {
+
+		repository.deleteAll();
 	}
 }
